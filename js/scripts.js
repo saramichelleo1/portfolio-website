b3dc701f0dd7f9179527d2aade66b9cef6dd4ac5
Sara Michelle Ouellet <saramichelleo@outlook.com>
2019-07-28 13:06:34 -0400

    javascript libraries

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! I'm Sara Michelle and I'm a UX Designer.")
+
+function menuToggle() {
+	var x = document.getElementById('myNavToggle');
+	if (x.className === 'responsive') {
+		x.className += 'responsive';
+	} else {
+		x.className = 'navtoggle';
+	}
+}
